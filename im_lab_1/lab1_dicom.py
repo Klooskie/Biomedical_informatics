d6be2f608bf71e8353f556352f288aa04332d2bb
Jakub Konieczny <kuba.koniecznyxx@gmail.com>
2020-03-07 19:39:32 +0100

    Added solution to task 2 of lab 1 (dicom image)

--- im_lab_1/lab1_dicom.py
+++ im_lab_1/lab1_dicom.py
@@ -2,17 +2,24 @@
 # Lab 1 : DICOM
 
 import pydicom
+import numpy as np
 from tkinter import *
 from PIL import Image, ImageTk
 
-class MainWindow():
 
+class MainWindow:
     ds = pydicom.dcmread("./data/head.dcm")
     data = ds.pixel_array
 
     def __init__(self, main):
         # print patient name
         print(self.ds.PatientName)
+
+        self.winCenter = self.ds.WindowCenter
+        self.winWidth = self.ds.WindowWidth
+
+        print(self.winWidth)
+        print(self.winCenter)
 
         # prepare canvas
         self.canvas = Canvas(main, width=512, height=512)
@@ -24,33 +31,36 @@ class MainWindow():
         self.canvas.bind("<ButtonRelease-3>", self.finishMeasure)
 
         # load image
-        # todo: apply transform
-        #self.array = self.transformData(self.data, self.winWidth, self.winCenter)
-        self.array = self.data
+        # apply transform
+        self.array = self.transformData(self.data, self.winWidth, self.winCenter)
+        # self.array = self.data
         self.image = Image.fromarray(self.array)
         self.image = self.image.resize((512, 512), Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(image=self.image, master=root)
-        self.image_on_canvas = self.canvas.create_image(0, 0, anchor = NW, image = self.img)
-
+        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=NW, image=self.img)
 
     def transformData(self, data, window, level):
-        # todo: transform data
+        window_start = level - window / 2
+        data = ((data - window_start) / window) * 255
         return data
 
-
     def initWindow(self, event):
-        # todo: save mouse position
+        # save mouse position
         print("x: " + str(event.x) + " y: " + str(event.y))
+        self.mouse_pos = event.x, event.y
 
     def updateWindow(self, event):
-        # todo: modify window width and center
+        # modify window width and center
         print("x: " + str(event.x) + " y: " + str(event.y))
-        #self.array2 = self.transformData(self.data, self.winWidth, self.winCenter)
-        #self.image2 = Image.fromarray(self.array2)
-        #self.image2 = self.image2.resize((512, 512), Image.ANTIALIAS)
-        #self.img2 = ImageTk.PhotoImage(image=self.image2, master=root)
-        #self.canvas.itemconfig(self.image_on_canvas, image = self.img2)
+        self.winCenter += self.mouse_pos[1] - event.y
+        self.winWidth += event.x - self.mouse_pos[0]
+        self.mouse_pos = event.x, event.y
 
+        self.array2 = self.transformData(self.data, self.winWidth, self.winCenter)
+        self.image2 = Image.fromarray(self.array2)
+        self.image2 = self.image2.resize((512, 512), Image.ANTIALIAS)
+        self.img2 = ImageTk.PhotoImage(image=self.image2, master=root)
+        self.canvas.itemconfig(self.image_on_canvas, image=self.img2)
 
     def initMeasure(self, event):
         # todo: save mouse position
@@ -68,8 +78,7 @@ class MainWindow():
         print("x: " + str(event.x) + " y: " + str(event.y))
 
 
-
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 
 root = Tk()
 MainWindow(root)
